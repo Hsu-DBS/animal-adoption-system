6a9febf40dd41dbdd446bda5063fc20038365d0b
Hsu-DBS <20084781@mydbs.ie>
2025-12-07 17:45:50 +0000

    :sparkles: Add API integration for viewing adopter applications list

--- client/my-react-app/src/api/applications.js
+++ client/my-react-app/src/api/applications.js
@@ -7,3 +7,11 @@ export async function submitApplication(payload) {
   const res = await api.post("/application-management/applications", payload);
   return res.data;
 }
+
+
+// GET applications for the current logged-in adopter
+export async function getMyApplications() {
+  // Send GET request
+  const res = await api.get("/application-management/applications/current-adopter");
+  return res.data.data.applications;
+}
